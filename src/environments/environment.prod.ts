859ca30bf2d4b8ba86114a295c90be4d1ec7d67c
PARKER BOWMAN <pbowman323@icloud.com>
2019-12-07 15:12:45 -0500

    initial shell app with firebase config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAYw_G5A0k2rqfcMYO0WVHrFTqAssRCQR0",
+    authDomain: "perfect-fit-923be.firebaseapp.com",
+    databaseURL: "https://perfect-fit-923be.firebaseio.com",
+    projectId: "perfect-fit-923be",
+    storageBucket: "perfect-fit-923be.appspot.com",
+    messagingSenderId: "1036630496196",
+    appId: "1:1036630496196:web:3dc1e7055ad2a2f8444a91",
+    measurementId: "G-88DBDHTDKZ"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAYw_G5A0k2rqfcMYO0WVHrFTqAssRCQR0",
+    authDomain: "perfect-fit-923be.firebaseapp.com",
+    databaseURL: "https://perfect-fit-923be.firebaseio.com",
+    projectId: "perfect-fit-923be",
+    storageBucket: "perfect-fit-923be.appspot.com",
+    messagingSenderId: "1036630496196",
+    appId: "1:1036630496196:web:3dc1e7055ad2a2f8444a91",
+    measurementId: "G-88DBDHTDKZ"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
